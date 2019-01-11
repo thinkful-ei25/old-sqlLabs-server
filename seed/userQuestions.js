c67887ea0cd0266416e2c04642b473022f01f6ca
Box <40639989+theBoxed@users.noreply.github.com>
2019-01-11 11:15:14 -0500

    completed features/users

--- seed/userQuestions.js
+++ seed/userQuestions.js
@@ -2,31 +2,26 @@
 
 const questions = [
   {
-    _id: '000000000000000000000001',
     questionText: 'Question 1',
     questionAnswer: '1',
     category: 'Basics'
   },
   {
-    _id: '000000000000000000000002',
     questionText: 'Question 2',
     questionAnswer: '2',
     category: 'Basics'
   },
   {
-    _id: '000000000000000000000003',
     questionText: 'Question 3',
     questionAnswer: '3',
     category: 'Basics'
   },
   {
-    _id: '000000000000000000000004',
     questionText: 'Question 4',
     questionAnswer: '4',
     category: 'Basics'
   },
   {
-    _id: '000000000000000000000005',
     questionText: 'Question 5',
     questionAnswer: '5',
     category: 'Basics'
